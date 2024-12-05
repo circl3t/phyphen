489cf291aaa14b3fdea10df637da4c25802b12ad
tsogtb <circl3t@gmail.com>
2024-12-05 12:44:20 +0100

    chore: Delete template code and Run ktfmt

--- composeApp/src/commonMain/kotlin/tb/circl3t/phyphen/Platform.kt
+++
@@ -1,7 +0,0 @@
-package tb.circl3t.phyphen
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/desktopMain/kotlin/tb/circl3t/phyphen/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package tb.circl3t.phyphen
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
