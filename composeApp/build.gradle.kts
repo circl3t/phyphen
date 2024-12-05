9679853c1a67ebd54ea5183ebeba04875cc094aa
tsogtb <circl3t@gmail.com>
2024-12-05 18:23:19 +0100

    build:@previous added testing dependencies for multiplatform

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -56,6 +56,7 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material)
             implementation(compose.ui)
+            // Enable access to resources
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
